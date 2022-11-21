e5fb36460f5ab4f9ab214564ce66d0970d47e7c5
Proust-stack <andrey-hunter@yandex.ru>
2022-11-21 12:00:06 +0400

    api changed

--- src/http/usersApi.js
+++ src/http/usersApi.js
@@ -2,15 +2,18 @@ import axios from "axios";
 
 export const getData = async (page, lan, seed, error) => {
   try {
-    const { data } = await axios.get(`http://localhost:5000/api/users`, {
-      params: {
-        page: page,
-        limit: page === 1 ? 20 : 10,
-        locale: lan,
-        seed,
-        error,
-      },
-    });
+    const { data } = await axios.get(
+      `https://fakedataitr.herokuapp.com/api/users`,
+      {
+        params: {
+          page: page,
+          limit: page === 1 ? 20 : 10,
+          locale: lan,
+          seed,
+          error,
+        },
+      }
+    );
     return data;
   } catch (error) {
     console.log(error);
